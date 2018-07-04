c043a6190de9fca17d09ab4ea25c3e14b60c30ba
Raul Villarreal <raulvilla0797@gmail.com>
2018-07-04 10:12:41 -0600

    Initial commit

---
+++ smack-app/smack-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  smack-app
+//
+//  Created by Raul Ernesto Villarreal Sigala on 7/4/18.
+//  Copyright © 2018 Raul Ernesto Villarreal Sigala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
