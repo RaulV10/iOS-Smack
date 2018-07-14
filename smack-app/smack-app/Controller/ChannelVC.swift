e960c7fc631194e8a8c400ab67c39598a4b9d4f7
Raul Villarreal <raulvilla0797@gmail.com>
2018-07-14 13:33:19 -0600

    Logging in users

--- smack-app/smack-app/Controller/ChannelVC.swift
+++ smack-app/smack-app/Controller/ChannelVC.swift
@@ -21,6 +21,10 @@ class ChannelVC: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        setupUserInfo()
+    }
+    
     @IBAction func btnLoginPressed(_ sender: Any) {
         if AuthService.instance.isLoggedIn {
             let profile = ProfileVC()
@@ -37,6 +41,10 @@ class ChannelVC: UIViewController {
     }
     
     @objc func userDataDidChange(_ notif: Notification) {
+        setupUserInfo()
+    }
+    
+    func setupUserInfo() {
         if AuthService.instance.isLoggedIn {
             btnPressed.setTitle(UserDataService.instance.name, for: .normal)
             userImage.image = UIImage(named: UserDataService.instance.avatarName)
--- smack-app/smack-app/Controller/LoginVC.swift
+++ smack-app/smack-app/Controller/LoginVC.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+    }
+    
+    func setupView() {
+        spinner.isHidden = true
+        
+        txtUsername.attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceHolder])
 
-        // Do any additional setup after loading the view.
+        txtPassword.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceHolder])
     }
     
     @IBAction func btnClosePressed(_ sender: Any) {
@@ -22,6 +34,27 @@ class LoginVC: UIViewController {
 
     @IBAction func btnCreateAccountPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
+    }
+    
+    @IBAction func btnLoginPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
+        
+        guard let email = txtUsername.text , txtUsername.text != "" else { return }
+        guard let password = txtPassword.text , txtPassword.text != "" else { return }
+        
+        AuthService.instance.loginUser(email: email, password: password) { (success) in
+            if success {
+                AuthService.instance.findUserByEmail(completion: { (success) in
+                    if success {
+                        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+                        self.spinner.isHidden = true
+                        self.spinner.stopAnimating()
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                })
+            }
+        }
     }
     
 }
